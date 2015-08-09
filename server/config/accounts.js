510afb2cd05a5d8a3f5e400a714f58f5e2f06601
Charles Zhu <charlesmzhu@gmail.com>
2015-08-09 01:10:33 -0700

    With email recurrence working

--- server/config/accounts.js
+++ server/config/accounts.js
@@ -7,21 +7,18 @@ Accounts.onCreateUser(function(options, user) {
   // we wait for Meteor to create the user before sending an email
   Meteor.setTimeout(function() { //Hacky. Why do we have to wait?
     Accounts.sendVerificationEmail ( user._id );
-  
-      console.log(savedHabits);
       user.profile = {};
       user.profile['currentHabits'] = savedHabits;
 
       var str = "Here are the habits that you chose:"
-      console.log(user);
       user.profile.currentHabits.forEach ( function (habit) {
       	str = str.concat("<br><br>", habit);
       	str = str.concat("<br><br>", "Were you able to do them today? :)");
       })
       
-      Meteor.call('scheduleEmail', {
-          from: emails.address,
-          to: emails.address,
+      Meteor.call('scheduleMail', {
+          from: user.emails.address,
+          to: user.emails.address,
           subject: "Are you closer to who you want to be?",
           text: str
       })
