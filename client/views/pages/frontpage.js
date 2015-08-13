0c32bebda4c1cce94cde19664718c6a7cea226c7
Walter Li <walterli@appnexus.com>
2015-08-13 01:20:16 -0400

    use underscore

--- client/views/pages/frontpage.js
+++ client/views/pages/frontpage.js
@@ -1,19 +1,27 @@
 Template.frontpage.rendered = function() {
-
+setTrait( "Healthy" );
 };
 
 Template.frontpage.helpers({
     traits: function(){
         return ["Healthy", "Organized", "Social", "Confident", "Thrifty"]; // If this is hardcoded, if there's a new category, it won't appear
     },
- 
+
     habits: function(){
       if( getTrait() ) {
-        return Habits.find( {'trait' : getTrait() } );
+        return Habits.find( {$and:[{'trait' : getTrait() }, {_id: {$nin: _.toArray(getSelectedHabits())}}]});
       } else {
         return;
       }
+    },
+
+    habitsSelected: function(){
+    if( getTrait() ) {
+      return Habits.find({_id: {$in: _.toArray(getSelectedHabits())}});
+    } else {
+      return;
     }
+  }
 
 });
 
@@ -22,7 +30,7 @@ Template.frontpage.events({
     e.preventDefault();
     var email = t.find('#login-email').value
       , password = t.find('#login-password').value; /* Eventually, this should be replaced with default loginButtons for security purposes but with custom template. See here https://meteorhacks.com/extending-meteor-accounts */
-      
+
     var habits = getSelectedHabits();
 
     Meteor.call ( 'saveSelectedHabits', habits );
@@ -47,31 +55,15 @@ Template.frontpage.events({
   },
 
   'click #habit-button': function(e, t) {
-
+    console.log(getSelectedHabits());
     var habit = Blaze.getData ( e.target ); // gets habit in object form
 
-    if($(e.currentTarget).hasClass("active")) {    
-    
-      var selectedHabits = getSelectedHabits(); // gets array of habits
-      
-      selectedHabits.splice ( selectedHabits.indexOf ( habit._id ), 1 ); // splice deselected habit
-
-      updateSelectedHabits ( selectedHabits );
+    if($(e.currentTarget).hasClass("active")) {
+      removeSelectedHabits(habit);
       $(e.currentTarget).removeClass("active"); // changes CSS
-    
     } else {
-
-      if ( doSelectedHabitsExist() ) {
-        var selectedHabits = getSelectedHabits();
-        selectedHabits.push ( habit._id );
-        updateSelectedHabits ( selectedHabits ); // At some point we'll need to get rid of all sessions  
-      } else {
-        var initHabit = [ habit._id ];
-        updateSelectedHabits ( initHabit );
-      }
-
+      addSelectedHabits(habit)
       $(e.currentTarget).addClass("active")
-
     }
   },
 
@@ -109,3 +101,23 @@ var updateSelectedHabits = function ( newSelectedHabits ) {
 var doSelectedHabitsExist = function () {
   return !!getSelectedHabits();
 }
+
+var removeSelectedHabits = function(habit) {
+  updateSelectedHabits(_.without(getSelectedHabits(),habit._id));
+}
+
+var addSelectedHabits = function(habit) {
+  if (doSelectedHabitsExist()) {
+    var selectedHabits = getSelectedHabits();
+    console.log(!_.contains(selectedHabits,habit._id))
+    if(!_.contains(selectedHabits,habit._id)) {
+      selectedHabits.push(habit._id )
+      updateSelectedHabits(selectedHabits); // At some point we'll need to get rid of all sessions
+    }
+  } else {
+    var initHabit = [habit._id];
+    updateSelectedHabits(initHabit);
+  }
+}
+
+
