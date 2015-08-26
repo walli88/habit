d5e266c33d2e2b81048bd54faa930ec60021b008
Walter Li <walterli@appnexus.com>
2015-08-25 23:20:51 -0400

    timestamp reset

--- client/views/pages/dashboard.js
+++ client/views/pages/dashboard.js
@@ -14,13 +14,36 @@ TOOD
 */
 
 Template.dashboard.events({
-    'change #yes' : function(e) {
+    'change #yes' : function(e,t) {
+        e.preventDefault();
         var userHabit = Blaze.getData(e.target);
-        Meteor.call('incUserHabitCount', this._id);
+        // console.log(e.target);
+        console.log($(e.target).parent());
+        // console.log($(e.currentTarget).closest("showBtn"));
+
+        var lastModify = moment.duration(moment(new Date()).diff(moment(userHabit.lastModified))).asSeconds();
+        console.log(lastModify);
+        if(lastModify < 2) {
+            console.log($(e.currentTarget));
+            $(e.currentTarget).attr('checked', false);
+            console.log($(e.currentTarget));
+            $(e.currentTarget).parent().removeClass("active");
+            sAlert.error('You can add at most 3 habits', {offset: '0px', stack: false})
+        } else {
+            Meteor.call('incUserHabitCount', this._id);
+        }
     },
     'change #no' : function(e) {
         var userHabit = Blaze.getData(e.target);
         Meteor.call('decUserHabitCount', this._id);
+        var lastModify = moment.duration(moment(new Date()).diff(moment(userHabit.lastModified))).asSeconds();
+        console.log(lastModify);
+        if(lastModify < 2) {
+            $(e.currentTarget).removeClass("active")
+            sAlert.error('You can add at most 3 habits', {offset: '0px', stack: false})
+        } else {
+            Meteor.call('decUserHabitCount', this._id);
+        }
     }
 
 });
