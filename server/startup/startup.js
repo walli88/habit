8470d5b744facf4af670ad413105eb4ca2ffa908
Walter Li <walterli@appnexus.com>
2015-08-27 23:28:58 -0400

    add mailgun conf

--- server/startup/startup.js
+++ server/startup/startup.js
@@ -35,6 +35,9 @@ Meteor.startup(function() {
 
   addTask("markFalseTask", 'every 1 day', markFalseTask)
 
+  process.env.MAIL_URL='smtp://postmaster%40sandbox2f612ffff1bc4c5aa681358f40348891.mailgun.org:4278877873ed0e12f6c1e219083570bb@smtp.mailgun.org:587';
+
 	SyncedCron.start();
+
 
 });
