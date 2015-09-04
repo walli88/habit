14221bd15ed4a09a83398ea17f3631cddd53ee0c
Walter Li <walterli@appnexus.com>
2015-09-04 01:12:41 -0400

    move scheduleMail back to onCreateUser

--- server/config/accounts.js
+++ server/config/accounts.js
@@ -3,11 +3,18 @@
 // ***************************************************************
 
 Accounts.onCreateUser(function(options, user) {
-  user.profile = {};
-//   // we wait for Meteor to create the user before sending an email
-  Meteor.setTimeout(function() { //Hacky. Why do we have to wait?
+    user.profile = {};
+    //   // we wait for Meteor to create the user before sending an email
+    Meteor.setTimeout(function() { //Hacky. Why do we have to wait?
     Accounts.sendVerificationEmail (user._id);
-  }, 2 * 1000);
+    }, 2 * 1000);
+
+    Meteor.call('scheduleMail', {
+        userId: user._id,
+        from: user.emails[0].address,
+        to: user.emails[0].address,
+        subject: "Are you closer to who you want to be?",
+    })
 
   return user;
 
